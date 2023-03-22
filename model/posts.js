badc528277e06ff0d3b6fb62946ce46a6985117e
Lucas Barbieri <lucas.barbieri@bitzar.solutions>
2023-03-22 17:48:25 -0300

    Projeto finalizado

---
+++ model/posts.js
@@ -0,0 +1,19 @@
+module.exports =  {
+    posts: [
+        {id:"aisdhoaid",
+        title:"Teste mural",
+        description:"Descrição teste"},
+    ],
+
+    getAll(){
+        return this.posts;
+    },
+
+    newPost(title,description){
+        this.posts.push({id: generateID,title,description});
+    }
+}
+
+function generateID () {
+    return Math.random().toString(36).substring(2,9);
+}
